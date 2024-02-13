379b4052ca78c5b2732636789523519a56878405
dwen3232 <davidrwen@gmail.com>
2024-02-12 20:21:51 -0500

    Changed some mappings to commands, removed some commands, too many things tied to <leader>t

--- nvim/lua/dwen/plugins/neotest.lua
+++ nvim/lua/dwen/plugins/neotest.lua
@@ -22,34 +22,43 @@ return {
     local keymap = vim.keymap -- for conciseness
     local opts = { noremap = true, silent = true }
 
-    opts.desc = "Run nearest test"
-    keymap.set("n", "<leader>tt", function()
-      neotest.run.run()
-    end, opts)
-
-    opts.desc = "Run the current file"
-    keymap.set("n", "<leader>tf", function()
-      neotest.run.run(vim.fn.expand("%"))
-    end, opts)
+    vim.cmd([[
+      command! NeotestSuite lua require("neotest").run.run({suite=true})
+      command! NeotestFile lua require("neotest").run.run(vim.fn.expand(%))
+      command! NeotestNearest lua require("neotest").run.run()
+      command! NeotestDebug lua require("neotest").run.run({ strategy = "dap" })
+      command! NeotestAttach lua require("neotest").run.attach()
+      command! NeotestStop lua require("neotest").run.stop()
+      command! NeotestOutput lua require("neotest").output.open({ enter = true, auto_close = true})
+      command! NeotestSummary lua require("neotest").summary.toggle()
+    ]])
+    -- opts.desc = "Run nearest test"
+    -- keymap.set("n", "<leader>tt", function()
+    --   neotest.run.run()
+    -- end, opts)
+    --
+    -- opts.desc = "Run the current file"
+    -- keymap.set("n", "<leader>tf", function()
+    --   neotest.run.run(vim.fn.expand("%"))
+    -- end, opts)
 
     opts.desc = "Open test output window"
     keymap.set("n", "<leader>to", function()
       neotest.output.open({ enter = true, auto_close = true })
     end, opts)
 
-    opts.desc = "Toggle test output panel"
-    keymap.set("n", "<leader>tp", function()
-      neotest.output_pannel.toggle()
-    end, opts)
-
-    opts.desc = "Stop nearest test"
+    opts.desc = "Toggle test summary window"
     keymap.set("n", "<leader>ts", function()
-      neotest.run.stop()
+      neotest.summary.toggle()
     end, opts)
-
-    opts.desc = "Attach to nearest test"
-    keymap.set("n", "<leader>ta", function()
-      neotest.run.attach()
-    end, opts)
+    -- opts.desc = "Stop nearest test"
+    -- keymap.set("n", "<leader>ts", function()
+    --   neotest.run.stop()
+    -- end, opts)
+    --
+    -- opts.desc = "Attach to nearest test"
+    -- keymap.set("n", "<leader>ta", function()
+    --   neotest.run.attach()
+    -- end, opts)
   end,
 }
