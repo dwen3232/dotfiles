5ae9754b4c6843fbdf218e2833a82f443de5af02
dwen3232 <davidrwen@gmail.com>
2024-02-13 21:08:21 -0500

    added alpha-nvim plugin

---
+++ nvim/lua/dwen/plugins/alpha-nvim.lua
@@ -0,0 +1,34 @@
+return {
+  "goolord/alpha-nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+    local fortune = require("alpha.fortune")
+
+    -- TODO: try out this tool and make some weeb art
+    -- https://github.com/Asthestarsfalll/img2art
+    dashboard.section.header.val = {
+      "                                                     ",
+      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+      "                                                     ",
+    }
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f", "󰦀  > Find file", ":cd $HOME | Telescope find_files<CR>"),
+      dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+      dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+      dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+    }
+    dashboard.section.footer.val = fortune()
+    alpha.setup(dashboard.opts)
+  end,
+}
