fb559b8a373aefc584e1bf6ac6631ed8685ea009
David Wen <davidrwen@gmail.com>
2024-08-17 10:24:20 -0400

    updated gitsigns

--- nvim/lua/dwen/plugins/coding/gitsigns.lua
+++ nvim/lua/dwen/plugins/coding/gitsigns.lua
@@ -1,13 +1,23 @@
+-- For displaying diff signs in buffer columns
 return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
   opts = {
     signs = {
-      add = { text = "+" },
-      change = { text = "~" },
+      add = { text = "|" },
+      change = { text = "|" },
       delete = { text = "_" },
       topdelete = { text = "‾" },
       changedelete = { text = "~" },
+      untracked = { text = "┆" },
+    },
+    signs_staged = {
+      add = { text = "|" },
+      change = { text = "|" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+      untracked = { text = "┆" },
     },
   },
   config = true,
