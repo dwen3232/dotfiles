d6030beac4284b423125a4f496c0faf3b7ef300e
David Wen <davidrwen@gmail.com>
2024-09-07 10:06:42 -0400

    Moved some stuff into testing

--- nvim/lua/dwen/plugins/testing/nvim-dap.lua
+++ nvim/lua/dwen/plugins/testing/nvim-dap.lua
@@ -5,12 +5,14 @@ return {
     "mfussenegger/nvim-dap-python",
   },
   config = function()
+    -- DAP Python
     local python_dap = require("dap-python")
     local debugpy_path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
     python_dap.setup(debugpy_path)
 
-    local keymap = vim.keymap -- for conciseness
 
+    -- Mappings
+    local keymap = vim.keymap -- for conciseness
     local opts = { noremap = true, silent = true }
 
     opts.desc = "Toggle breakpoint"
