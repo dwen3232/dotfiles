266a0347d00cc64144c505fd016774599ce51088
David Wen <davidrwen@gmail.com>
2024-10-13 17:44:52 -0400

    Tweaked image.nvim, this probably won't be in use until I set up molten.nvim

--- dot-config/nvim/lua/dwen/plugins/editor/image.lua
+++ dot-config/nvim/lua/dwen/plugins/editor/image.lua
@@ -3,11 +3,26 @@ return {
   "3rd/image.nvim",
   opts = {
     backend = "kitty", -- whatever backend you would like to use
-    max_width = 100,
-    max_height = 12,
+    integrations = {
+      markdown = {
+        enabled = true,
+        clear_in_insert_mode = false,
+        download_remote_images = true,
+        only_render_image_at_cursor = false,
+        filetypes = { "markdown" }, -- markdown extensions (ie. quarto) can go here
+      },
+      html = {
+        enabled = true,
+      },
+    },
+    max_width = nil,
+    max_height = nil,
     max_height_window_percentage = math.huge,
     max_width_window_percentage = math.huge,
     window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
     window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+    editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+    tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+    hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
   },
 }
