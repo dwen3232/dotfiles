d9a169784235d9a127493f6b06669fda7a6a6f97
David Wen <dwen@lahzo.com>
2026-01-20 15:00:06 -0500

    let's try not using this for a bit, it's kinda annoying sometimes

--- dot-config/nvim/lua/dwen/core/keymaps.lua
+++ dot-config/nvim/lua/dwen/core/keymaps.lua
@@ -9,8 +9,8 @@ vim.g.loaded_netrwPlugin = 1
 -- TODO: add tab switching keybindings?
 
 -- Let's center after moving a half page
-vim.api.nvim_set_keymap("n", "<C-D>", "<C-D>zz", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-U>", "<C-U>zz", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<C-D>", "<C-D>zz", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<C-U>", "<C-U>zz", { noremap = true, silent = true })
 
 -- Putting this here since file types with no LSP may stil have diagnostics (due to linters)
 vim.keymap.set(
